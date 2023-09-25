da7440ea7ffbd3265692508013e296c5b0f43b6d
Magic8763 <114419914+Magic8763@users.noreply.github.com>
2023-09-25 13:11:59 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -652,10 +652,10 @@ def handle_message(event):
 	thread.start()
 	"""
 
-Read_All_Data2()
-KNN_Recommend()
 if __name__ == "__main__": # 當app.py是被執行而非被引用時, 執行下列程式碼
 	print("\n######### main ########")
+	Read_All_Data2()
+	KNN_Recommend()
 	port = int(os.environ.get('PORT', 5000))
 	app.run(host='0.0.0.0', port = port) # 以linebot()接收請求
 """
